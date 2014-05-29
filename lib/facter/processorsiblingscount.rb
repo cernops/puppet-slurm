0d206fa72450ec0b7e44d00da1091e90cbde1d40
Trey Dockendorf <treydock@gmail.com>
2014-05-29 13:53:30 -0500

    Add new fact, processorsiblingscount and update rspec tests for existing facts

---
+++ lib/facter/processorsiblingscount.rb
@@ -0,0 +1,28 @@
+# Fact: processorsiblingscount
+#
+# Purpose: Return the number of siblings per physical processor
+#
+# Caveats: It assumes homogeneous physical CPUs
+#
+
+require 'facter/util/file_read'
+
+default_siblings_count = 1
+source = '/proc/cpuinfo'
+
+Facter.add('processorsiblingscount') do
+  confine :kernel => :linux
+
+  if File.exists?(source)
+    output = Facter::Util::FileRead.read(source)
+    result = output[/^siblings.*(\d+)/, 1]
+
+    setcode do
+      if result.nil?
+        default_siblings_count
+      else
+        result.to_i
+      end
+    end
+  end
+end
--- spec/unit/facter/threadcountpercore_spec.rb
+++ spec/unit/facter/threadcountpercore_spec.rb
@@ -9,23 +9,23 @@ describe 'threadcountpercore Fact' do
     File.stubs(:exists?).with('/proc/cpuinfo').returns(true)
   end
 
-  it "should return 2" do
-    cpuinfo = cpuinfo_fixture_read('xeon_E52667v2_dualsocket_cpuinfo')
-    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture_readlines('xeon_E52667v2_dualsocket_cpuinfo'))
-    Facter::Util::FileRead.stubs(:read).with('/proc/cpuinfo').returns(cpuinfo_fixture_read('xeon_E52667v2_dualsocket_cpuinfo'))
-    Facter.fact(:physicalprocessorcount).stubs(:value).returns(2)
-    Facter.fact(:processorcount).stubs(:value).returns(32)
-    Facter.fact(:corecountpercpu).stubs(:value).returns(8)
-    Facter.fact(:threadcountpercore).value.should == 2
-  end
-
   it "should return 1" do
-    cpuinfo = cpuinfo_fixture_read('xeon_E52667v2_dualsocket_cpuinfo')
-    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo_fixture_readlines('xeon_E52667v2_dualsocket_cpuinfo'))
-    Facter::Util::FileRead.stubs(:read).with('/proc/cpuinfo').returns(cpuinfo_fixture_read('xeon_E52667v2_dualsocket_cpuinfo'))
+    cpuinfo = cpuinfo_fixture_read('xeon_E5420_dualsocket_cpuinfo')
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo)
+    Facter::Util::FileRead.stubs(:read).with('/proc/cpuinfo').returns(cpuinfo)
     Facter.fact(:physicalprocessorcount).stubs(:value).returns(2)
     Facter.fact(:processorcount).stubs(:value).returns(8)
     Facter.fact(:corecountpercpu).stubs(:value).returns(4)
+    Facter.fact(:threadcountpercore).value.should == 1
+  end
+
+  it "should return 1" do
+    cpuinfo = cpuinfo_fixture_read('amd_opteron_6320_quadsocket_cpuinfo')
+    File.stubs(:readlines).with("/proc/cpuinfo").returns(cpuinfo)
+    Facter::Util::FileRead.stubs(:read).with('/proc/cpuinfo').returns(cpuinfo)
+    Facter.fact(:physicalprocessorcount).stubs(:value).returns(4)
+    Facter.fact(:processorcount).stubs(:value).returns(32)
+    Facter.fact(:corecountpercpu).stubs(:value).returns(8)
     Facter.fact(:threadcountpercore).value.should == 1
   end
 end
