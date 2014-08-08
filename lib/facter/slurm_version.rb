0576b95b80212b7cfaea501eb9f243097d177934
Trey Dockendorf <treydock@gmail.com>
2014-08-08 18:01:49 -0500

    * Add slurm_version Fact

---
+++ lib/facter/slurm_version.rb
@@ -0,0 +1,11 @@
+# slurm_version.rb
+
+Facter.add(:slurm_version) do
+  setcode do
+    if sinfo = Facter::Util::Resolution.which("sinfo")
+      if sinfo_v = Facter::Util::Resolution.exec("sinfo -V").match(/^slurm (.*)$/)
+        result = sinfo_v[1]
+      end
+    end
+  end
+end
---
+++ spec/unit/facter/slurm_version_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe 'slurm_version fact' do
+  before :each do
+    Facter::Util::Resolution.stubs(:which).with("sinfo").returns("/usr/bin/sinfo")
+    Facter.clear
+  end
+
+  it "should return 14.03.6" do
+    Facter::Util::Resolution.stubs(:exec).with("sinfo -V").returns("slurm 14.03.6")
+    Facter.fact(:slurm_version).value.should == "14.03.6"
+  end
+
+  it "should be nil if sinfo -V returns unexpected output" do
+    Facter::Util::Resolution.stubs(:exec).with("sinfo -V").returns("foo")
+    Facter.fact(:slurm_version).value.should == nil
+  end
+
+  it "should be nil if sinfo not found" do
+    Facter::Util::Resolution.stubs(:which).with("sinfo").returns(nil)
+    Facter.fact(:slurm_version).value.should == nil
+  end
+end
