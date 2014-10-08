5f35a8124ccaabe7d3e724f3846a809dd6fa01a5
Trey Dockendorf <treydock@gmail.com>
2014-10-08 15:17:33 -0500

    Supress stderr when getting the slurm_version fact

--- lib/facter/slurm_version.rb
+++ lib/facter/slurm_version.rb
@@ -3,7 +3,7 @@
 Facter.add(:slurm_version) do
   setcode do
     if sinfo = Facter::Util::Resolution.which("sinfo")
-      if sinfo_v = Facter::Util::Resolution.exec("sinfo -V").match(/^slurm (.*)$/)
+      if sinfo_v = Facter::Util::Resolution.exec("sinfo -V 2>/dev/null").match(/^slurm (.*)$/)
         result = sinfo_v[1]
       end
     end
--- spec/unit/facter/slurm_version_spec.rb
+++ spec/unit/facter/slurm_version_spec.rb
@@ -7,12 +7,12 @@ describe 'slurm_version fact' do
   end
 
   it "should return 14.03.6" do
-    Facter::Util::Resolution.stubs(:exec).with("sinfo -V").returns("slurm 14.03.6")
+    Facter::Util::Resolution.stubs(:exec).with("sinfo -V 2>/dev/null").returns("slurm 14.03.6")
     Facter.fact(:slurm_version).value.should == "14.03.6"
   end
 
   it "should be nil if sinfo -V returns unexpected output" do
-    Facter::Util::Resolution.stubs(:exec).with("sinfo -V").returns("foo")
+    Facter::Util::Resolution.stubs(:exec).with("sinfo -V 2>/dev/null").returns("foo")
     Facter.fact(:slurm_version).value.should == nil
   end
 
