6bea8643bfbf4ad1c61bd4ffdc79941ceaf54bfe
Philippe Ganz <philippe.yves.ganz@cern.ch>
2016-11-21 16:20:32 +0100

    added secrets

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,18 +1,20 @@
 #
 # slurm/config.pp
-#   creates the basic folders and user/group for SLURM common to headnodes and
-#   workernodes
+#   creates the basic folders, user/group and security for SLURM, common to
+#   headnodes and workernodes
 #
 
 class slurm::config (
-  $slurm_homefolder = '/var/lib/slurm',
-  $slurm_gid        = '950',
-  $slurm_uid        = '950',
+  $homefolder = '/usr/local/slurm',
+  $slurm_gid  = '950',
+  $slurm_uid  = '950',
+  $key_priv   = 'slurmkey',
+  $key_pub    = 'slurmcert',
 ) {
 
   file{ 'slurm home folder':
     ensure => directory,
-    path   => $slurm_homefolder,
+    path   => $homefolder,
     group  => 'slurm',
     mode   => '1755',
     owner  => 'slurm',
@@ -26,9 +28,33 @@ class slurm::config (
     ensure  => present,
     comment => 'SLURM workload manager',
     gid     => 'slurm',
-    home    => $slurm_homefolder,
+    home    => $homefolder,
     require => [Group['glexec'], File['slurm home folder']],
     system  => true,
     uid     => $slurm_uid,
+  }
+
+  file{ 'credentials folder':
+    ensure => directory,
+    path   => "${homefolder}/credentials",
+    group  => 'slurm',
+    mode   => '1755',
+    owner  => 'slurm',
+  }
+  teigi::secret{ 'slurm private key':
+    key     => 'slurmkey',
+    path    => "${homefolder}/credentials/slurm.key",
+    owner   => 'slurm',
+    group   => 'slurm',
+    mode    => '0644',
+    require => File['credentials folder'],
+  }
+  teigi::secret{ 'slurm public key':
+    key     => 'slurmcert',
+    path    => "${homefolder}/credentials/slurm.cert",
+    owner   => 'slurm',
+    group   => 'slurm',
+    mode    => '0600',
+    require => File['credentials folder'],
   }
 }
