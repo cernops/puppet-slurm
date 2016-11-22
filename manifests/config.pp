a0b48425de2ba342a0202f00b4491060dca54005
Philippe Ganz <philippe.yves.ganz@cern.ch>
2016-11-22 09:21:54 +0100

    added parameters for template

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,20 @@
 #
 # slurm/config.pp
-#   
+#
 #
 
 class slurm::config (
-
+  $headnode       = 'slurm01.cern.ch',
+  $failover       = 'slurm02.cern.ch',
+  $slurmkey       = '/usr/local/slurm/slurm.key',
+  $slurmcert      = '/usr/local/slurm/slurm.cert',
+  $slurmctld_port = '6817',
+  $slurmd_port    = '6818',
+  $slurmuser      = 'slurm',
+  $scheduler_port = '7321',
+  $slurmctld_log  = '/var/log/slurmctld.log',
+  $slurmd_log     = '/var/log/slurmd.log',
+  $partitionname  = 'batch-testing'
 ) {
 
   file{ 'common configuration file':
