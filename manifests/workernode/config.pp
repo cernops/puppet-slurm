5f976821e134e861274006fc05c2e62df8f7cc1c
Carolina Lindqvist <carolina.lindqvist@cern.ch>
2016-11-23 07:47:52 +0100

    initial WN config

--- manifests/workernode/config.pp
+++ manifests/workernode/config.pp
@@ -4,7 +4,11 @@
 #   modified
 #
 
-class slurm::workernode::config {
+class slurm::workernode::config($workernodes = []) {
+
+  # PuppetDB lookup for many nodes.. check if OK.
+  $hostgroup = 'bi/hpc/batch/workernode'
+  $workernodes =  sort(unique(query_nodes("hostgroup=\"$hostgroup\"", 'fqdn')))
 
   service{'slurmd':
     ensure    => running,
