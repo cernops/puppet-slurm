d70eecd5fb51c5a042d1649340f76a3f9ac7fb4a
Carolina Lindqvist <carolina.lindqvist@cern.ch>
2017-02-14 10:11:43 +0100

    Update job_stuck_alert.sh

--- files/job_stuck_alert.sh
+++ files/job_stuck_alert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps aux | grep mpi && echo "A Slurm job was canceled, but it is stuck at $HOSTNAME, and cannot be killed. Please investigate." | /usr/bin/mutt -s "Slurm job stuck at $HOSTNAME" -c nils.hoimyr@cern.ch -c philippe.yves.ganz@cern.ch carolina.lindqvist@cern.ch
+ps aux | grep mpi && echo "A Slurm job should have stopped, but it is stuck at $HOSTNAME, and cannot be killed. Please investigate." | /usr/bin/mutt -s "Slurm job stuck at $HOSTNAME" -c nils.hoimyr@cern.ch -c philippe.yves.ganz@cern.ch carolina.lindqvist@cern.ch
