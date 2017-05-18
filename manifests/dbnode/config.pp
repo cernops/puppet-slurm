6d2a3b97622d0e01952c11031d09eb7b568b04c9
Philippe Ganz <philippe.yves.ganz@cern.ch>
2017-05-18 14:56:27 +0200

    Change variable documentation and default value in dbnode/config.pp

--- manifests/dbnode/config.pp
+++ manifests/dbnode/config.pp
@@ -3,15 +3,15 @@
 # Creates the slurmdb configuration file and ensures that the slurmdbd
 # service is running and restarted if the configuration file is modified
 #
+# @param file_name The name of the temporary dbd file before being processed by the password manager or manually copied and modified; if you intend to use a clear password in the storage_pass variable, which is NOT recommanded, using the default slurmdbd.conf as file name is enough
 # @param dbd_host The short, or long, name of the machine where the Slurm Database Daemon is executed
 # @param dbd_port The port number that the Slurm Database Daemon (slurmdbd) listens to for work
 # @param slurm_user The name of the user that the slurmctld daemon executes as
 # @param storage_host Define the name of the host the database is running where we are going to store the data
-# @param storage_pass The password identifier to access the database; it should NOT contain the actual password but an identifier later processed by a secret management system, e.g. done in the hostgroup
+# @param storage_pass The the database password identifier; it should NOT contain the actual password but an identifier later changed
 # @param storage_port The port number that the Slurm Database Daemon (slurmdbd) communicates with the database
 # @param storage_user Define the name of the user we are going to connect to the database with to store the job accounting data
 # @param storage_loc Specify the name of the database as the location where accounting records are written
-# @param file_name The name of the dbd file before being processed by the password manager; if you intend to use a clear password in the storage_pass variable, which is NOT recommanded, using the default slurmdbd.conf as file name is enough
 #
 # version 20170518
 #
@@ -28,7 +28,7 @@ class slurm::dbnode::config (
   String $slurm_user    = $slurm::config::slurm_user,
   String $storage_host  = 'db_instance.example.org',
   Integer $storage_port = 1234,
-  String $storage_pass  = 'your_secure_password_identifier',
+  String $storage_pass  = 'CHANGEME__storage_pass',
   String $storage_user  = 'user',
   String $storage_loc   = 'accountingdb',
 ) {
