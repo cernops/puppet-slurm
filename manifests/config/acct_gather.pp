a2f3b1a442bd7878a7f53a3d1b2da37e4d0187ed
Philippe Ganz <philippe.yves.ganz@cern.ch>
2017-06-20 11:26:19 +0200

    Add a dedicated class for acct_gather
     * Add all parameters available for AcctGather plugins with default values from SLURM documentation
    
     * Use conditionnals to comment out unused parameters

---
+++ manifests/config/acct_gather.pp
@@ -0,0 +1,45 @@
+# slurm/config/acct_gather.pp
+#
+# Creates the acct_gather plugin configuration files.
+#
+# @param energy_ipmi_frequency This parameter is the number of seconds between BMC access samples.
+# @param energy_ipmi_calc_adjustment If set to "yes", the consumption between the last BMC access sample and a step consumption update is approximated to get more accurate task consumption.
+# @param energy_ipmi_power_sensors Optionally specify the ids of the sensors to used.
+# @param energy_ipmi_username Specify BMC Username.
+# @param energy_ipmi_password Specify BMC Password.
+# @param profile_hdf5_dir This parameter is the path to the shared folder into which the acct_gather_profile plugin will write detailed data (usually as an HDF5 file).
+# @param profile_hdf5_default A comma delimited list of data types to be collected for each job submission.
+# @param infiniband_ofed_port This parameter represents the port number of the local Infiniband card that we are willing to monitor.
+#
+# version 20170602
+#
+# Copyright (c) CERN, 2016-2017
+# Authors: - Philippe Ganz <phganz@cern.ch>
+#          - Carolina Lindqvist <calindqv@cern.ch>
+# License: GNU GPL v3 or later.
+#
+
+class slurm::config::acct_gather (
+  Boolean $with_energy_ipmi = false,
+  Integer[0,default] $energy_ipmi_frequency = 10,
+  Enum['no','yes'] $energy_ipmi_calc_adjustment = 'no',
+  Hash[String,String] $energy_ipmi_power_sensors = {},
+  String[0,default] $energy_ipmi_username = '',
+  String[0,default] $energy_ipmi_password = '',
+  Boolean $with_profile_hdf5 = false,
+  String[0,default] $profile_hdf5_dir = '',
+  String[1,default] $profile_hdf5_default = 'None',
+  Boolean $with_infiniband_ofed = false,
+  Integer[0,default] $infiniband_ofed_port = 1,
+) {
+
+  # AcctGather* plugin configuration
+  file{'/etc/slurm/acct_gather.conf':
+    ensure  => file,
+    content => template('slurm/acct_gather.conf.erb'),
+    owner   => 'slurm',
+    group   => 'slurm',
+    mode    => '0644',
+    require => User['slurm'],
+  }
+}
