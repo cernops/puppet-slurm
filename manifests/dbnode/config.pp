4db1832afa787b9bf12afadb009cfd2f0419140e
Philippe Ganz <philippe.yves.ganz@cern.ch>
2017-06-20 11:37:07 +0200

    Improve the dbnode configuration class
     * Add all parameters available for DB plugins with default values from SLURM documentation
    
     * Use conditionnals to comment out unused parameters
    
     * Use strong types to validate user input

--- manifests/dbnode/config.pp
+++ manifests/dbnode/config.pp
@@ -1,19 +1,10 @@
 # slurm/dbnode/config.pp
 #
-# Creates the slurmdb configuration file and ensures that the slurmdbd
-# service is running and restarted if the configuration file is modified
+# Creates the slurmdb configuration file and ensures that the slurmdbd service is running and restarted if the configuration file is modified.
 #
-# @param file_name The name of the temporary dbd file before being processed by a secrets manager or manually copied and modified; if you intend to use a cleartext password in the storage_pass variable, which is NOT recommended, using the default slurmdbd.conf as file name is enough
-# @param dbd_host The short, or long, name of the machine where the Slurm Database Daemon is executed
-# @param dbd_port The port number that the Slurm Database Daemon (slurmdbd) listens to for work
-# @param slurm_user The name of the user that the slurmctld daemon executes as
-# @param storage_host Define the name of the host the database is running where we are going to store the data
-# @param storage_pass The the database password identifier; it should NOT contain the actual password but an identifier (key) that a secrets manager can replace with the correct value (password)
-# @param storage_port The port number that the Slurm Database Daemon (slurmdbd) communicates with the database
-# @param storage_user Define the name of the user we are going to connect to the database with to store the job accounting data
-# @param storage_loc Specify the name of the database as the location where accounting records are written
+# For details about the parameters, please refer to the SLURM documentation at https://slurm.schedmd.com/slurmdbd.conf.html
 #
-# version 20170518
+# version 20170620
 #
 # Copyright (c) CERN, 2016-2017
 # Authors: - Philippe Ganz <phganz@cern.ch>
@@ -22,15 +13,49 @@
 #
 
 class slurm::dbnode::config (
-  String $file_name     = 'slurmdbd.conf',
-  String $dbd_host      = 'localhost',
-  Integer $dbd_port     = $slurm::config::accounting_storage_port,
-  String $slurm_user    = $slurm::config::slurm_user,
-  String $storage_host  = 'db_instance.example.org',
-  Integer $storage_port = 1234,
-  String $storage_pass  = 'CHANGEME__storage_pass',
-  String $storage_user  = 'user',
-  String $storage_loc   = 'accountingdb',
+  String[1,default] $file_name = 'slurmdbd.conf',
+  String[1,default] $dbd_host = 'localhost',
+  String[1,default] $dbd_addr = $dbd_host,
+  String[0,default] $dbd_backup_host = '',
+  Integer[0,default] $dbd_port = $slurm::config::accounting_storage_port,
+  Enum['auth/none','auth/munge'] $auth_type = 'auth/none',
+  String[0,default] $auth_info = '',
+  String[0,default] $default_qos = '',
+  Integer[0,default] $message_timeout = 10,
+  String[1,default] $pid_file = '/var/run/slurmdbd.pid',
+  String[1,default] $plugin_dir = '/usr/local/lib/slurm' ,
+  String[0,default] $private_data = '',
+  String[1,default] $slurm_user = $slurm::config::slurm_user,
+  Integer[0,default] $tcp_timeout = 2,
+  Enum['no','yes'] $track_wc_key = 'no',
+  Enum['no','yes'] $track_slurmctld_down = 'no',
+  Enum['accounting_storage/mysql'] $storage_type = 'accounting_storage/mysql',
+  String[1,default] $storage_host = 'db_instance.example.org',
+  String[0,default] $storage_backup_host = '',
+  Integer[0,default] $storage_port = 1234,
+  String[1,default] $storage_user = 'user',
+  String[1,default] $storage_pass = 'CHANGEME__storage_pass',
+  String[1,default] $storage_loc = 'accountingdb',
+  String[1,default] $archive_dir = '/tmp',
+  String[0,default] $archive_script = '',
+  Enum['no','yes'] $archive_events = 'no',
+  String[0,default] $purge_event_after = '',
+  Enum['no','yes'] $archive_jobs = 'no',
+  String[0,default] $purge_job_after = '',
+  Enum['no','yes'] $archive_resvs = 'no',
+  String[0,default] $purge_resv_after = '',
+  Enum['no','yes'] $archive_steps = 'no',
+  String[0,default] $purge_step_after = '',
+  Enum['no','yes'] $archive_suspend = 'no',
+  String[0,default] $purge_suspend_after = '',
+  Enum['no','yes'] $archive_txn = 'no',
+  String[0,default] $purge_txnafter = '',
+  Enum['no','yes'] $archive_usage = 'no',
+  String[0,default] $purge_usage_after = '',
+  Enum['quiet','fatal','error','info','verbose','debug','debug2','debug3','debug4','debug5'] $debug_level = 'info',
+  Array[Enum['DB_ARCHIVE','DB_ASSOC','DB_EVENT','DB_JOB','DB_QOS','DB_QUERY','DB_RESERVATION','DB_RESOURCE','DB_STEP','DB_USAGE','DB_WCKEY']] $debug_flags = [],
+  String[0,default] $log_file = '',
+  Enum['iso8601','iso8601_ms','rfc5424','rfc5424_ms','clock','short'] $log_time_format = 'iso8601_ms',
 ) {
 
   file{ "/etc/slurm/${file_name}":
@@ -38,7 +63,7 @@ class slurm::dbnode::config (
     content => template('slurm/slurmdbd.conf.erb'),
     owner   => 'slurm',
     group   => 'slurm',
-    mode    => '0644',
+    mode    => '0400',
     require => User['slurm'],
   }
 
